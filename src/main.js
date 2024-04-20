ba287fb455fa93b08b7771bf419d4162aff3fe18
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-04-20 23:54:47 +0000

    wip:  add starter impl in main.js

--- src/main.js
+++ src/main.js
@@ -1,28 +1,56 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
-
+const { mkdir, stat, rm, cp, writeFile, readFile } = require('node:fs/promises')
+const { resolve, join } = require('node:path')
+const { platform } = require('node:os')
+const { execSync } = require('node:child_process')
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds', { required: true })
+  const seaConfigPath = core.getInput('sea-config-path')
+  const workingDir = core.getInput('working-dir')
+  const outputPath = core.getInput('output-dir')
+  const executableName = core.getInput('executable-name')
+  const appPath = core.getInput('path-to-app')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    core.setFailed(error.message)
+  const os = platform()
+  const buildFolder = resolve(join(process.cwd(), outputPath))
+  await mkdir(resolve(join(process.cwd(), buildFolder)))
+
+  const seaJsonPath = resolve(join(workingDir, seaConfigPath))
+  execSync(`node --experimental-sea-config ${seaJsonPath}`)
+
+  const pathToNode = resolve(process.argv[0])
+  let nodeDest = join(buildFolder, executableName)
+
+  if (os === 'win32' && !executableName.endsWith('.exe')) {
+    nodeDest += '.exe'
   }
+
+  await cp(pathToNode, nodeDest)
+
+  // remove existing code signature on node binary
+  if (os === 'win32') {
+    execSync(`signtool remove /s ${nodeDest}`)
+  } else if (os === 'darwin') {
+    execSync(`codesign --remove-signature ${nodeDest}`)
+  }
+  const seaConfigContents = await readFile(seaJsonPath, { 
+    encoding: 'utf8'
+  })
+  const seaConfig = json.Parse(seaConfigContents)
+
+  const blobPath = seaConfig.output
+
+  // TODO: switch to postject api call
+  // if (os === 'darwin') {
+  //   execSync(`npx postject ${nodeDest} NODE_SEA_BLOB ${blobPath} --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 --macho-segment-name NODE_SEA`)
+  // } else {
+  //   execSync(`npx postject ${nodeDest} NODE_SEA_BLOB ${blobPath} --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2`)
+  // }
 }
 
 module.exports = {
