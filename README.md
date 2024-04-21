5782ad0572e82c817701cfccaa3d168e4b939b88
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-04-21 14:19:38 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,68 @@
+# Node.JS SEA (Single Executable Application) Action
+
+[![GitHub Super-Linter](https://github.com/bryopsida/node-sea-action/actions/workflows/linter.yml/badge.svg)](https://github.com/super-linter/super-linter)
+![CI](https://github.com/bryopsida/node-sea-action/actions/workflows/ci.yml/badge.svg)
+
+## What does this do?
+
+This action takes a bundled JS file and creates a Node.JS SEA.
+
+## What does this not do?
+
+- Bundle your JS, you need to bundle your application into a single js file with
+  something like esbuild
+- Support importing/requiring npm modules, everything needs to be baked into the
+  bundle, you can include additional assets available through the SEA api but
+  thats it.
+- Sign Code, it takes the steps to strip the original Node.JS signature to give
+  a blank canvas for signing but leaves code signing, if needed, to you.
+- Cross Compilation, the SEA generated will match the arch of the github runner,
+  if you need arm64, you'll need to run the action on a arm64 runner.
+
+## Usage
+
+To include the action in a workflow in another repository, you can use the
+`uses` syntax with the `@` symbol to reference a specific branch, tag, or commit
+hash.
+
+```yaml
+sea-action:
+  name: Build SEA
+  strategy:
+    matrix:
+      os: [ubuntu-latest]
+  runs-on: ${{ matrix.os }}
+  steps:
+    - name: Checkout
+      id: checkout
+      uses: actions/checkout@v4
+
+    - name: Setup Node.js
+      id: setup-node
+      uses: actions/setup-node@v4
+      with:
+        node-version-file: .node-version
+        cache: npm
+
+    - name: Find Node
+      id: find-node
+      run:
+        echo "node=$(node -e 'console.log(process.argv[0]);')" >>
+        "$GITHUB_OUTPUT"
+
+    - name: SEA
+      id: sea
+      uses: ./
+      with:
+        working-dir: .
+        output-dir: build
+        executable-name: sea
+        sea-config-path: test-app/sea-config.json
+        node-path: ${{ steps.find-node.outputs.node }}
+
+    - uses: actions/upload-artifact@v4
+      with:
+        name: ${{ matrix.os }}-sea
+        path: build/
+        if-no-files-found: error
+```
---
+++ src/main.js
@@ -0,0 +1,69 @@
+const core = require('@actions/core')
+const { mkdir, cp, readFile } = require('node:fs/promises')
+const { resolve, join } = require('node:path')
+const { platform } = require('node:os')
+const { execSync } = require('node:child_process')
+const { inject } = require('postject')
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+async function run() {
+  const seaConfigPath = core.getInput('sea-config-path')
+  const workingDir = core.getInput('working-dir')
+  const outputPath = core.getInput('output-dir')
+  const executableName = core.getInput('executable-name')
+  const inputNodePath = core.getInput('node-path')
+  const pathToNode =
+    inputNodePath !== '' ? resolve(inputNodePath) : resolve(process.argv[0])
+
+  core.info(`Runtime Node version is: ${process.version}`)
+  core.info(`Node path is: ${pathToNode}`)
+
+  const os = platform()
+  core.info(`OS Platform is: ${os}`)
+  const buildFolder = resolve(join(process.cwd(), outputPath))
+  await mkdir(buildFolder, {
+    recursive: true
+  })
+
+  const seaJsonPath = resolve(join(workingDir, seaConfigPath))
+  execSync(`${pathToNode} --experimental-sea-config ${seaJsonPath}`)
+
+  let nodeDest = join(buildFolder, executableName)
+
+  if (os === 'win32' && !executableName.endsWith('.exe')) {
+    nodeDest += '.exe'
+  }
+
+  await cp(pathToNode, nodeDest)
+
+  // remove existing code signature on node binary
+  if (os === 'win32') {
+    const signtool =
+      '%programfiles(x86)%/Windows Kits/10/bin/10.0.17763.0/x86/signtool.exe'
+    execSync(`"${signtool}" remove /s ${nodeDest}`)
+  } else if (os === 'darwin') {
+    execSync(`codesign --remove-signature ${nodeDest}`)
+  }
+  const seaConfigContents = await readFile(seaJsonPath, {
+    encoding: 'utf8'
+  })
+  const seaConfig = JSON.parse(seaConfigContents)
+
+  const blobPath = seaConfig.output
+
+  const opts = {
+    sentinelFuse: 'NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2'
+  }
+  if (os === 'darwin') {
+    opts.machoSegmentName = 'NODE_SEA'
+  }
+  const resourceBlob = await readFile(blobPath)
+  await inject(nodeDest, 'NODE_SEA_BLOB', resourceBlob, opts)
+}
+
+module.exports = {
+  run
+}
